3bdf7d74738aa5b2d1c879b880df581959e609e6
Josue <josueisaiasmartinezlopez@gmail.com>
2021-10-12 21:34:09 -0600

    Agregando el ejercicio 1

---
+++ ejercicio1.cpp
@@ -0,0 +1,29 @@
+/*
+Programador: Josue Isaias Martinez
+Fecha: 12/10/2021
+Descripcion:
+Seleccione y escriba el bucle adecuado que mejor 
+resuelva las siguientes tareas:
+
+a) Suma de series tales como 1/2 + 1/3 + 1/4 + 
++ 1/5 + ... + 1/50
+Respuesta: Un bucle for
+
+b) Lectura de la lista de calificaciones de un examen de Historia
+Respuesta: Un bucle while
+
+
+c) Visualizar la suma de enteros en el intervalo 
+11...50
+Respuesta: Un bucle for, se inicializa i con el valor de 11
+
+*/
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+
+    return 0;
+}
